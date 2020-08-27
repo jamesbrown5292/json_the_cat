48fd0825b6ec6069b93c3d04be23e8859fe7f0a1
James Brown <jamesbrown5292@gmail.com>
2020-08-27 20:15:36 +0000

    added command line search string input functionality

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,16 @@
 const request = require('request');
 const fs = require('fs');
+//allow the user to specify the breed name using command line arguments
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib'
+const input = process.argv[2];
+
+
+//Make a request to the server, set error, status and body parameters; parse body as JSON and return relevant information
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + input;
+console.log(url);
 request(url, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   const data = JSON.parse(body);
-  console.log(data, typeof data);
+  console.log(data[0].description);
 });
