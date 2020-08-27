e09a7cd6d638c2e0c92b47d015068b9e35d89f86
James Brown <jamesbrown5292@gmail.com>
2020-08-27 20:34:05 +0000

    handled page not found and linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,15 +6,19 @@ const input = process.argv[2];
 
 
 //Make a request to the server, set error, status and body parameters; parse body as JSON and return relevant information
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + input;
+const url = 'https://appi.thecatapi.com/v1/breeds/search?q=' + input;
 console.log(url);
 request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (!data.length) {
-    console.log('We couldn\'t find that breed.');
+  if (error.code === 'ENOTFOUND') {
+    console.log('We couldn\'t find that page');
   } else {
-    console.log(data[0].description);
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    const data = JSON.parse(body);
+    //handle the case where the page is not found
+    if (!data.length) {
+      console.log('We couldn\'t find that breed.');
+    } else {
+      console.log(data[0].description);
+    }
   }
 });
