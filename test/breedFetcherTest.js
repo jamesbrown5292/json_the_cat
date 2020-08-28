55937d2a5a0b99658e9e3b5b54e376f9f3bf647c
James Brown <jamesbrown5292@gmail.com>
2020-08-28 02:47:48 +0000

    implemented tests and linted

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -12,18 +12,22 @@ describe('fetchBreedDescription', () => {
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ";
 
       // compare returned description
-      console.log(typeof desc)
+      console.log(typeof desc);
       assert.equal(expectedDesc, desc);
 
       done();
     });
-  it('returns an error for a non existent breed', (done) => {
-    fetchBreedDescription('Sibby', (err, desc) => {
-      // we expect no error for this scenario
+  });
+  it('returns an error message for an invalid breed', (done) => {
+    fetchBreedDescription('Sibellian', (err, desc) => {
+      // we expect no description for this scenario
       assert.equal(desc, null);
-      const errorMessage = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ";
-      // compare returned description
+
+      const errorMessage = "We couldn't find that breed.";
+
+      // compare returned error
       assert.equal(errorMessage, err);
+
       done();
     });
   });
